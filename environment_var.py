6d2908548504878850d212449724098c8dc16336
ayashvanth <mr.abhishek.y@gmail.com>
2026-02-06 14:25:15 +0530

    ruff

--- environment_var.py
+++ environment_var.py
@@ -1,11 +1,19 @@
 import os
+
+import pandas
 from dotenv import load_dotenv
+
+print(type(pandas))  # Packages - find under .venv/lib
+print(type(os))  # module - system-path
+
+print(os.__file__)
+# /Library/Frameworks/Python.framework/Versions/3.14/lib/python3.14/os.py
 
 # Reading the .env file
 load_dotenv()
 
-api_key = os.environ.get('API_KEY')
-debugMode = os.environ.get('DEBUG')
+api_key = os.environ.get("API_KEY")
+debugMode = os.environ.get("DEBUG")
 
 print(api_key)
 print(debugMode)
